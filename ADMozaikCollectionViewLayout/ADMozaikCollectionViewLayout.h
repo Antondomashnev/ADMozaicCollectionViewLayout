d3c4ee77396a81a99d98f8e19a00b123642b166c
Anton Domashnev <antondomashnev@gmail.com>
2016-05-16 16:45:26 +0200

    Initial Commit

---
+++ ADMozaikCollectionViewLayout/ADMozaikCollectionViewLayout.h
@@ -0,0 +1,19 @@
+//
+//  ADMozaikCollectionViewLayout.h
+//  ADMozaikCollectionViewLayout
+//
+//  Created by Anton Domashnev on 16/05/16.
+//  Copyright © 2016 Anton Domashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ADMozaikCollectionViewLayout.
+FOUNDATION_EXPORT double ADMozaikCollectionViewLayoutVersionNumber;
+
+//! Project version string for ADMozaikCollectionViewLayout.
+FOUNDATION_EXPORT const unsigned char ADMozaikCollectionViewLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADMozaikCollectionViewLayout/PublicHeader.h>
+
+
