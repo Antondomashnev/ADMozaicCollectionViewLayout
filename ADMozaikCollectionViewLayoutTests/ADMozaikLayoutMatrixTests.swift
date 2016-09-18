5a39bc33460ef67e6168383a1ae0c3594475cd67
Anton <Antondomashnev@gmail.com>
2016-09-18 21:25:51 +0200

    Swift 3.0 compatibility (#8)
    Swift 3.0 compatibility;

--- ADMozaikCollectionViewLayoutTests/ADMozaikLayoutMatrixTests.swift
+++ ADMozaikCollectionViewLayoutTests/ADMozaikLayoutMatrixTests.swift
@@ -26,47 +26,47 @@ class ADMozaikLayoutMatrixTests: XCTestCase {
     }
     
     func testThatEmptyMatrixShouldReturnTopLeftPositionForNewItem() {
-        let position: ADMozaikLayoutPosition = self.matrixLayout.positionForItemWithSize(ADMozaikLayoutSize(columns: 2, rows: 2))!
+        let position: ADMozaikLayoutPosition = self.matrixLayout.positionForItem(withSize: ADMozaikLayoutSize(numberOfColumns: 2, numberOfRows: 2))!
         expect(position.column).to(equal(0))
         expect(position.row).to(equal(0))
     }
     
     func testThatEmptyMatrixShouldReturnNilPositionForNewItemWiderThanPossible() {
-        let position: ADMozaikLayoutPosition? = self.matrixLayout.positionForItemWithSize(ADMozaikLayoutSize(columns: 4, rows: 2))
+        let position: ADMozaikLayoutPosition? = self.matrixLayout.positionForItem(withSize: ADMozaikLayoutSize(numberOfColumns: 4, numberOfRows: 2))
         expect(position).to(beNil())
     }
     
     func testThatEmptyMatrixShouldReturnNilPositionForNewItemHigherThanPossible() {
-        let position: ADMozaikLayoutPosition? = self.matrixLayout.positionForItemWithSize(ADMozaikLayoutSize(columns: 2, rows: 5))
+        let position: ADMozaikLayoutPosition? = self.matrixLayout.positionForItem(withSize: ADMozaikLayoutSize(numberOfColumns: 2, numberOfRows: 5))
         expect(position).to(beNil())
     }
     
     func testThatFilledMatrixShouldReturnNilPositionIfTherIsNoSpaceLeft() {
         do {
-            try self.matrixLayout.addItemWithSize(ADMozaikLayoutSize(columns: 3, rows: 3), atPosition: ADMozaikLayoutPosition(column: 0, row: 0))
-            try self.matrixLayout.addItemWithSize(ADMozaikLayoutSize(columns: 1, rows: 1), atPosition: ADMozaikLayoutPosition(column: 0, row: 3))
+            try self.matrixLayout.addItem(withSize: ADMozaikLayoutSize(numberOfColumns: 3, numberOfRows: 3), at: ADMozaikLayoutPosition(atColumn: 0, atRow: 0))
+            try self.matrixLayout.addItem(withSize: ADMozaikLayoutSize(numberOfColumns: 1, numberOfRows: 1), at: ADMozaikLayoutPosition(atColumn: 0, atRow: 3))
         }
         catch {
             
         }
-        let position: ADMozaikLayoutPosition? = self.matrixLayout.positionForItemWithSize(ADMozaikLayoutSize(columns: 2, rows: 2))
+        let position: ADMozaikLayoutPosition? = self.matrixLayout.positionForItem(withSize: ADMozaikLayoutSize(numberOfColumns: 2, numberOfRows: 2))
         expect(position).to(beNil())
     }
     
     func testThatPositionForItemWithSizeShouldReturnCorrectPosition() {
         do {
-            try self.matrixLayout.addItemWithSize(ADMozaikLayoutSize(columns: 1, rows: 1), atPosition: ADMozaikLayoutPosition(column: 0, row: 0))
+            try self.matrixLayout.addItem(withSize: ADMozaikLayoutSize(numberOfColumns: 1, numberOfRows: 1), at: ADMozaikLayoutPosition(atColumn: 0, atRow: 0))
         }
         catch {}
-        var nextPosition = self.matrixLayout.positionForItemWithSize(ADMozaikLayoutSize(columns: 3, rows: 2))!
+        var nextPosition = self.matrixLayout.positionForItem(withSize: ADMozaikLayoutSize(numberOfColumns: 3, numberOfRows: 2))!
         expect(nextPosition.row).to(equal(1))
         expect(nextPosition.column).to(equal(0))
         
         do {
-            try self.matrixLayout.addItemWithSize(ADMozaikLayoutSize(columns: 3, rows: 2), atPosition: nextPosition)
+            try self.matrixLayout.addItem(withSize: ADMozaikLayoutSize(numberOfColumns: 3, numberOfRows: 2), at: nextPosition)
         }
         catch {}
-        nextPosition = self.matrixLayout.positionForItemWithSize(ADMozaikLayoutSize(columns: 2, rows: 1))!
+        nextPosition = self.matrixLayout.positionForItem(withSize: ADMozaikLayoutSize(numberOfColumns: 2, numberOfRows: 1))!
         expect(nextPosition.row).to(equal(0))
         expect(nextPosition.column).to(equal(1))
     }
