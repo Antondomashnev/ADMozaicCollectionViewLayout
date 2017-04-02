0b6d8d8d64911c97a7762fbf9eb55d785024eaca
Anton Domashnev <antondomashnev@gmail.com>
2017-04-02 12:43:38 +0200

    Add multiple sections support;

--- ADMozaikCollectionViewLayoutTests/ADMozaikLayoutTests.swift
+++ ADMozaikCollectionViewLayoutTests/ADMozaikLayoutTests.swift
@@ -34,6 +34,12 @@ enum ADMozaikLayoutTestsUseCase {
     /// Use case when there are no sections
     case g
     
+    /// Use case when there are two sections with f & f use cases
+    case h
+    
+    /// Use case when there are three sections with different use cases (a, c, e)
+    case i
+    
     var minimumLineSpacing: CGFloat {
         switch self {
         case .a:
@@ -49,6 +55,10 @@ enum ADMozaikLayoutTestsUseCase {
         case .f:
             return 1
         case .g:
+            return 0
+        case .h:
+            return ADMozaikLayoutTestsUseCase.f.minimumLineSpacing
+        case .i:
             return 0
         }
     }
@@ -69,6 +79,10 @@ enum ADMozaikLayoutTestsUseCase {
             return 1
         case .g:
             return 1
+        case .h:
+            return ADMozaikLayoutTestsUseCase.f.minimumInteritemSpacing
+        case.i:
+            return 0
         }
     }
     
@@ -88,6 +102,10 @@ enum ADMozaikLayoutTestsUseCase {
             return [ADMozaikLayoutColumn(width: 63), ADMozaikLayoutColumn(width: 63), ADMozaikLayoutColumn(width: 64), ADMozaikLayoutColumn(width: 63), ADMozaikLayoutColumn(width: 63)]
         case .g:
             return [ADMozaikLayoutColumn(width: 107), ADMozaikLayoutColumn(width: 106), ADMozaikLayoutColumn(width: 107)]
+        case .h:
+            return ADMozaikLayoutTestsUseCase.f.columns
+        case .i:
+            return []
         }
     }
     
@@ -107,6 +125,10 @@ enum ADMozaikLayoutTestsUseCase {
             return 63
         case .g:
             return 107
+        case .h:
+            return ADMozaikLayoutTestsUseCase.f.rowHeight
+        case .i:
+            return 0
         }
     }
     
@@ -126,6 +148,42 @@ enum ADMozaikLayoutTestsUseCase {
             return 40
         case .g:
             return 40
+        case .h:
+            return 10
+        case .i:
+            return 5
+        }
+    }
+    
+    var numberOfSections: Int {
+        switch self {
+        case .a:
+            return 1
+        case .b:
+            return 1
+        case .c:
+            return 1
+        case .d:
+            return 1
+        case .e:
+            return 1
+        case .f:
+            return 1
+        case .g:
+            return 0
+        case .h:
+            return 2
+        case .i:
+            return 3
+        }
+    }
+    
+    var underlyingUseCases: [ADMozaikLayoutTestsUseCase]? {
+        switch self {
+        case .i:
+            return [ADMozaikLayoutTestsUseCase.a, ADMozaikLayoutTestsUseCase.c, ADMozaikLayoutTestsUseCase.e]
+        default:
+            return nil
         }
     }
 }
@@ -134,21 +192,41 @@ class ADMozaikLayoutTestsViewController: UIViewController, ADMozaikLayoutDelegat
     
     var collectionView: UICollectionView!
     var useCase: ADMozaikLayoutTestsUseCase!
-    var colors: [UIColor]! = []
+    var colors: [[UIColor]]! = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setUpCollectionView()
         
-        let availableColors = [UIColor(red: 0.09803921569, green: 0.462745098, blue: 0.8235294118, alpha: 1),
-                               UIColor(red: 0.1294117647, green: 0.5882352941, blue: 0.9529411765, alpha: 1),
-                               UIColor(red: 0.7333333333, green: 0.8705882353, blue: 0.9843137255, alpha: 1),
-                               UIColor(red: 0.3764705882, green: 0.4901960784, blue: 0.5450980392, alpha: 1),
-                               UIColor(red: 0.1294117647, green: 0.1294117647, blue: 0.1294117647, alpha: 1),
-                               UIColor(red: 0.4470588235, green: 0.4470588235, blue: 0.4470588235, alpha: 1),
-                               UIColor(red: 0.7137254902, green: 0.7137254902, blue: 0.7137254902, alpha: 1)]
-        for itemNumber in 0..<self.useCase.numberOfItems {
-            self.colors.append(availableColors[itemNumber % availableColors.count])
+        let availableColors = [
+                               [UIColor(red: 0.09803921569, green: 0.462745098, blue: 0.8235294118, alpha: 1),
+                                UIColor(red: 0.1294117647, green: 0.5882352941, blue: 0.9529411765, alpha: 1),
+                                UIColor(red: 0.7333333333, green: 0.8705882353, blue: 0.9843137255, alpha: 1),
+                                UIColor(red: 0.3764705882, green: 0.4901960784, blue: 0.5450980392, alpha: 1),
+                                UIColor(red: 0.1294117647, green: 0.1294117647, blue: 0.1294117647, alpha: 1),
+                                UIColor(red: 0.4470588235, green: 0.4470588235, blue: 0.4470588235, alpha: 1),
+                                UIColor(red: 0.7137254902, green: 0.7137254902, blue: 0.7137254902, alpha: 1)],
+                               [UIColor(red: 0.9882352941, green: 0.3960784314, blue: 0.2745098039, alpha: 1),
+                                UIColor(red: 0.8549019608, green: 0.0823529411, blue: 0.2313725490, alpha: 1),
+                                UIColor(red: 0.8823529412, green: 0.5058823529, blue: 0.4705882353, alpha: 1),
+                                UIColor(red: 0.3725490196, green: 0.1490196078, blue: 0.08235294118, alpha: 1),
+                                UIColor(red: 0.431372549, green: 0.1607843137, blue: 0.1882352941, alpha: 1),
+                                UIColor(red: 0.8901960784, green: 0.1764705882, blue: 0.2980392157, alpha: 1),
+                                UIColor(red: 0.4901960784, green: 0.1921568627, blue: 0.231372549, alpha: 1)],
+                               [UIColor(red: 0.9333333333, green: 0.6078431373, blue: 0.05882352941, alpha: 1),
+                                UIColor(red: 1, green: 0.8470588235, blue: 0.4588235294, alpha: 1),
+                                UIColor(red: 0.9960784314, green: 0.9333333333, blue: 0.4705882353, alpha: 1),
+                                UIColor(red: 1, green: 0.7294117647, blue: 0, alpha: 1),
+                                UIColor(red: 1, green: 0.8745098039, blue: 0, alpha: 1),
+                                UIColor(red: 1, green: 0.9647058824, blue: 0.4901960784, alpha: 1),
+                                UIColor(red: 1, green: 0.7450980392, blue: 0, alpha: 1)]
+                              ]
+        for section in 0..<self.useCase.numberOfSections {
+            var colorsInSection: [UIColor] = []
+            for itemNumber in 0..<self.useCase.numberOfItems {
+                colorsInSection.append(availableColors[section][itemNumber % availableColors[section].count])
+            }
+            colors.append(colorsInSection)
         }
     }
     
@@ -165,12 +243,22 @@ class ADMozaikLayoutTestsViewController: UIViewController, ADMozaikLayoutDelegat
     //MARK: - ADMozaikLayoutDelegate
     
     func collectonView(_ collectionView: UICollectionView, mozaik layoyt: ADMozaikLayout, geometryInfoFor section: ADMozaikLayoutSection) -> ADMozaikLayoutSectionGeometryInfo {
+        if let underlyingUseCase = self.useCase.underlyingUseCases?[section] {
+            return ADMozaikLayoutSectionGeometryInfo(rowHeight: underlyingUseCase.rowHeight, columns: underlyingUseCase.columns, minimumInteritemSpacing: underlyingUseCase.minimumInteritemSpacing, minimumLineSpacing: underlyingUseCase.minimumLineSpacing)
+        }
         return ADMozaikLayoutSectionGeometryInfo(rowHeight: self.useCase.rowHeight, columns: self.useCase.columns, minimumInteritemSpacing: self.useCase.minimumInteritemSpacing, minimumLineSpacing: self.useCase.minimumLineSpacing)
     }
     
     func collectionView(_ collectionView: UICollectionView, mozaik layout: ADMozaikLayout, mozaikSizeForItemAt indexPath: IndexPath) -> ADMozaikLayoutSize {
-        switch self.useCase! {
+        var useCase = self.useCase!
+        if let underlyingUseCase = self.useCase.underlyingUseCases?[indexPath.section] {
+            useCase = underlyingUseCase
+        }
+        
+        switch useCase {
         case .f:
+            fallthrough
+        case .h:
             if (indexPath as NSIndexPath).item % 8 == 0 {
                 return ADMozaikLayoutSize(numberOfColumns: 2, numberOfRows: 2)
             }
@@ -192,12 +280,7 @@ class ADMozaikLayoutTestsViewController: UIViewController, ADMozaikLayoutDelegat
     //MARK: - UICollectionViewDataSource
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        switch self.useCase! {
-        case .g:
-            return 0
-        default:
-            return 1
-        }
+        return self.useCase!.numberOfSections
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -206,7 +289,7 @@ class ADMozaikLayoutTestsViewController: UIViewController, ADMozaikLayoutDelegat
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ADMozaikLayoutCell", for: indexPath) as UICollectionViewCell
-        cell.backgroundColor = self.colors[(indexPath as NSIndexPath).item]
+        cell.backgroundColor = self.colors[indexPath.section][(indexPath as NSIndexPath).item]
         return cell
     }
     
@@ -276,6 +359,24 @@ class ADMozaikLayoutSnapshotTests: FBSnapshotTestCase {
     func testADMozaikLayoutRepresentationWithComplexLayout() {
         self.viewController = ADMozaikLayoutTestsViewController()
         self.viewController.useCase = .f
+        self.viewController.view.frame = self.testingFrame()
+        self.viewController.collectionView.reloadData()
+        
+        self.FBSnapshotVerifyView(self.viewController.view)
+    }
+    
+    func testADMozaikLayoutRepresentationWithTwoSectionsWithSameComplexLayout() {
+        self.viewController = ADMozaikLayoutTestsViewController()
+        self.viewController.useCase = .h
+        self.viewController.view.frame = self.testingFrame()
+        self.viewController.collectionView.reloadData()
+        
+        self.FBSnapshotVerifyView(self.viewController.view)
+    }
+    
+    func testADMozaikLayoutRepresentationWithThreeSectionsWithDifferentLayout() {
+        self.viewController = ADMozaikLayoutTestsViewController()
+        self.viewController.useCase = .i
         self.viewController.view.frame = self.testingFrame()
         self.viewController.collectionView.reloadData()
         
